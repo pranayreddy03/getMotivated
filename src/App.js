da3cc52cbf05e4095c82002fce315c20da56bf7f
pranayreddy03 <reddy.pranay03@gmail.com>
2019-12-26 10:26:54 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>get Motivated</h1>
+      <button className="btn btn-primary">button</button>
+    </div>
+  );
+}
+
+export default App;
